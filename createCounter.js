2e42bf50c3f5188e4d01e26fb8c16c8f0c0b57dd
Murage-Ivy <ivymurage2000@gmail.com>
2024-04-10 21:16:55 +0200

    create a createcounter function

--- createCounter.js
+++ createCounter.js
@@ -1,10 +1,9 @@
 function createCounter() {
-    let res = 0;
-    return function counterFn() {
-      return res++;
-    };
-  }
-  
-  const counter = createCounter();
-  Array.from({ length: 5 }, () => console.log(counter())); // 0 1 2 3 4
-  
+  let res = 0;
+  return function counterFn() {
+    return res++;
+  };
+}
+
+const counter = createCounter();
+Array.from({ length: 5 }, () => console.log(counter())); // 0 1 2 3 4
